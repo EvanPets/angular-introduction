93e3c541b20ff6cf4dbac74c38b06ce9f93e6b24
Evangelos Petsalis <vagelis.pets@gmail.com>
2024-04-02 22:23:24 +0300

    angular-material

--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,17 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EPerson, Person } from 'src/app/shared/interfaces/person';
 
 @Component({
   selector: 'app-person-table',
   standalone: true,
   imports: [],
   templateUrl: './person-table.component.html',
-  styleUrl: './person-table.component.css'
+  styleUrl: './person-table.component.css',
 })
 export class PersonTableComponent {
-  person = {
-    givenName: 'Vangelis',
-    surname: 'Petsalis',
-    age: 32,
-    email: 'vagelispets@aueb.gr'
-  };
+  @Input() person: Person | EPerson | undefined;
+
+  isPerson() {
+    return this.person && 'address' in this.person;
+  }
+
+  isEPerson() {
+    return this.person && 'education' in this.person;
+  }
 }
